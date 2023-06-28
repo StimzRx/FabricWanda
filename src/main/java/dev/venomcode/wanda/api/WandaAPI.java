4bed045cbee648af7e3c308edcb0905fc7b3dfde
VenomCode <103589496+VenomCodeDev@users.noreply.github.com>
2023-06-28 05:04:04 -0500

    Updated icon, serverapi version, javadocs on API, and contact in the fabric.mod.json file.

--- src/main/java/dev/venomcode/wanda/api/WandaAPI.java
+++ src/main/java/dev/venomcode/wanda/api/WandaAPI.java
@@ -4,8 +4,14 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Pair;
 import net.minecraft.util.math.BlockPos;
 
+/**
+ * API for the Wanda point-selection library/tool
+ */
 public class WandaAPI {
-    public Pair<BlockPos, BlockPos> getPlayersSelected(ServerPlayerEntity serverPlayer) {
+    /**
+     * Gets a players selection in BlockPos format as a pair of PRIMARY, SECONDARY
+     */
+    public Pair<BlockPos, BlockPos> getPlayerSelection(ServerPlayerEntity serverPlayer) {
         IWandaPlayer wandaPlayer = (IWandaPlayer) serverPlayer;
         return new Pair<>(wandaPlayer.getSelectedPrimary(), wandaPlayer.getSelectedSecondary());
     }
